fea64893a7381281fff0d2fb719203f64f928e42
Ereb-999 <Naofumi999@yandex.ru>
2023-09-22 22:58:38 +0400

    Convection chapter 2

--- src/Main.java
+++ src/Main.java
@@ -87,5 +87,90 @@ public class Main {
                 System.out.println("Число " + three + " наибольшее среди трёх чисел.");
             }
         }
+
+        short clientOS = 1;
+        short clientDeviceYear = 2013;
+        if (clientOS == 1 && clientOS > 0) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+
+        short year = 2040;
+        if ((year > 1584) && (year %4 == 0) && (year %100 != 0) && (year %400 == 0)) {
+            System.out.println(year + " - год являеться високостным.");
+        } else {
+            System.out.println(year + " - год не являеться високостным.");
+        }
+
+        short deliveryDistance = 95;
+        if (deliveryDistance == 20) {
+            System.out.println("Потребуеться дней: " + 1);
+        }
+        if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуеться дней: " + 2);
+        }
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуеться дней: " + 3);
+        }
+        if (deliveryDistance > 100) {
+            System.out.println("Доставки нет");
+        }
+
+        char monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Январь, зимний месяц.");
+                break;
+            case 2:
+                System.out.println("Февраль, зимний месяц.");
+                break;
+            case 3:
+                System.out.println("Март, весенний месяц.");
+                break;
+            case 4:
+                System.out.println("Апрель, весенний месяц.");
+                break;
+            case 5:
+                System.out.println("Май, весенний месяц.");
+                break;
+            case 6:
+                System.out.println("Июнь, летний месяц.");
+                break;
+            case 7:
+                System.out.println("Июль, летний месяц.");
+                break;
+            case 8:
+                System.out.println("Август, летний месяц.");
+                break;
+            case 9:
+                System.out.println("Сентябрь, осенний месяц.");
+                break;
+            case 10:
+                System.out.println("Октябрь, осенний месяц.");
+                break;
+            case 11:
+                System.out.println("Ноябрь, осенний месяц.");
+                break;
+            case 12:
+                System.out.println("Декабрь, зимний месяц.");
+                break;
+            default:
+                System.out.println("Такого месяца нет.");
+
+
+
+
+
+        }
+
+
+
     }
 }
